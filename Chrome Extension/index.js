6c389102063adbbb81b6d1b4f6c3cbc88871a639
Guilherme Pinheiro <gui.pinheiro@outlook.com>
2022-03-24 15:05:01 -0300

    Added Chrome Extension functionality: save input as a list

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -1,10 +1,36 @@
 const inputBtn = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
+const ulEl = document.querySelector("#ul-el");
 
-let myleads = []; 
-let myInput = inputEl.textContent;
+let myLeads = [];
 
-//Using event listernet instead of html onclick atribute  
-inputBtn.addEventListener("click", function() {
-        alert(myInput);
-    });
+// ? Necessary?
+ulEl.innerHTML = "";
+inputEl.value = "";
+
+//Using event listener instead of html onclick atribute
+//Adds user text to a list
+inputBtn.addEventListener("click", () => {
+    if (inputEl.value != "") {
+        let userInput = inputEl.value;
+
+        myLeads.push(userInput);
+
+        let newItem = document.createElement("li");
+        let newLink = document.createElement("a");
+        newLink.innerHTML = userInput;
+        newLink.href = userInput;
+        newLink.target = "_blank";
+        newItem.appendChild(newLink);
+        ulEl.appendChild(newItem);
+        inputEl.value = "";
+    }
+
+    //! not good practice. Reloads the whole HTML (might destroy events, is slow)
+    ////ulEl.innerHTML += `<li>${inputEl.value}</li>`;
+});
+
+// testing
+//// myLeads.forEach((leads) => {
+////     ulEl.innerHTML += `<li>${leads}</li>`;
+////});
