888e0b281661752df5876b485ecb7f211150c35b
Guilherme Pinheiro <gui.pinheiro@outlook.com>
2022-03-25 12:04:44 -0300

    New functionality: When reloading the extension, localStorage will be used to retrieve older inputs from the user

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -8,29 +8,41 @@ let myLeads = [];
 ulEl.innerHTML = "";
 inputEl.value = "";
 
+//Increase the list of leads with user inputs
+const renderLeads = (userInput) => {
+    let newItem = document.createElement("li");
+    let newLink = document.createElement("a");
+    newLink.innerHTML = userInput;
+    newLink.href = userInput;
+    newLink.target = "_blank";
+    newItem.appendChild(newLink);
+    ulEl.appendChild(newItem);
+};
+
+// Renders local inputs saved in previous sessions
+const renderLocal = () => {
+    myLeads = JSON.parse(localStorage.getItem("myLeads"));
+    myLeads.forEach((element) => {
+        renderLeads(element);
+    });
+};
+
+if (localStorage.getItem("myLeads") != null) {
+    renderLocal(); //If any local storage already has inputs, display them when reloading the page
+}
+
 //Using event listener instead of html onclick atribute
 //Adds user text to a list
 inputBtn.addEventListener("click", () => {
     if (inputEl.value != "") {
         let userInput = inputEl.value;
+        inputEl.value = "";
 
         myLeads.push(userInput);
 
-        let newItem = document.createElement("li");
-        let newLink = document.createElement("a");
-        newLink.innerHTML = userInput;
-        newLink.href = userInput;
-        newLink.target = "_blank";
-        newItem.appendChild(newLink);
-        ulEl.appendChild(newItem);
-        inputEl.value = "";
+        //Sets in local storage new inputs
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+
+        renderLeads(userInput);
     }
-
-    //! not good practice. Reloads the whole HTML (might destroy events, is slow)
-    ////ulEl.innerHTML += `<li>${inputEl.value}</li>`;
 });
-
-// testing
-//// myLeads.forEach((leads) => {
-////     ulEl.innerHTML += `<li>${leads}</li>`;
-////});
