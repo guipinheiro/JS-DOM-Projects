f8ec1f47638fed685bcdcd55c719a95f708823ca
Guilherme Pinheiro <gui.pinheiro@outlook.com>
2022-03-27 21:34:07 -0300

    Updated the manifest and created new functionallity: get current tab url and save it to the list of leads

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -1,5 +1,6 @@
-const inputBtn = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
+const inputBtn = document.querySelector("#input-btn");
+const tabBtn = document.querySelector("#tab-btn");
 const clearBtn = document.querySelector("#clear-btn");
 const ulEl = document.querySelector("#ul-el");
 
@@ -22,7 +23,7 @@ const render = (userInput) => {
     ulEl.appendChild(newItem);
 };
 
-// Renders local inputs saved in previous sessions
+// Renders local inputs saved in previous sessions and return an array with localStorage items
 const renderLocal = () => {
     let leads = JSON.parse(localLeads);
     leads.forEach((element) => {
@@ -31,14 +32,14 @@ const renderLocal = () => {
     return leads;
 };
 
-//If any local storage already has inputs, display them when reloading the page
+//If any local storage already has inputs, display them when reloading the page and return the array to myLeads
 if (localLeads) {
     myLeads = renderLocal();
 }
 
 //Adds user text to a list
 inputBtn.addEventListener("click", () => {
-    if (inputEl.value != "") {
+    if (inputEl.value) {
         let userInput = inputEl.value;
         inputEl.value = "";
 
@@ -51,13 +52,27 @@ inputBtn.addEventListener("click", () => {
     }
 });
 
-//Clear localStorage history and list of leads
-clearBtn.addEventListener("click", () => {
-    const result = window.confirm("Are you sure you want to clear your leads?");
+tabBtn.addEventListener("click", () => {
+    let queryOptions = { active: true, currentWindow: true };
+    chrome.tabs.query(queryOptions, function (tabs) {
+        let tabLead = tabs[0].url;
+        myLeads.push(tabLead);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(tabLead);
+    });
+});
 
-    if (result) {
-        localStorage.removeItem("myLeads");
-        ulEl.innerHTML = "";
-        myLeads = [];
+// Clear localStorage history and list of leads
+clearBtn.addEventListener("click", () => {
+    if (myLeads[0]) {
+        const result = window.confirm(
+            "Are you sure you want to clear your leads?"
+        );
+        if (result) {
+            localStorage.removeItem("myLeads");
+            localLeads = localStorage.getItem("myLeads");
+            ulEl.innerHTML = "";
+            myLeads = [];
+        }
     }
 });
